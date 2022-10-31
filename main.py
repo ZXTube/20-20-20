e25aff9f6b01bfcf314b6a6544cf5691cfaaa9dc
ZXTube <ziyadedris@gmail.com>
2022-10-31 16:09:59 +0000

    Added Sound Effect

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pygame
 import time
 
 pygame.font.init()
+pygame.mixer.init()
 
 WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 WIDTH, HEIGHT = WIN.get_size()
 HALF_WIDTH, HALF_HEIGHT = WIDTH/2, HEIGHT/2
 
-font = pygame.font.SysFont('comicsans', 250)
+FINISHED_SFX = pygame.mixer.Sound('Resources/Finished.wav')
+FONT = pygame.font.SysFont('comicsans', 250)
 LIGHT_GRAY = (200, 200, 200)
 DARK_GRAY = (30, 30, 30)
 
@@ -22,8 +24,8 @@ def main():
     clock = pygame.time.Clock()
     startingTime = time.time()
 
-    timer = font.render('20', True, LIGHT_GRAY)
-    timerValue = 20
+    timer = FONT.render('22', True, LIGHT_GRAY)
+    timerValue = 22
 
     halfTimerWidth, halfTimerHeight = timer.get_size()
     halfTimerHeight /= 2
@@ -42,13 +44,17 @@ def main():
                 running = False
 
         timerValue -= 1
-        timer = font.render(str(timerValue), True, LIGHT_GRAY)
+        timer = FONT.render(str(timerValue), True, LIGHT_GRAY)
         timerPos[0] = HALF_WIDTH - timer.get_width() / 2
 
         if timerValue == 0:
             running = False
 
         draw(timer, timerPos)
+
+    pygame.display.quit()
+    pygame.mixer.Sound.play(FINISHED_SFX)
+    time.sleep(1)
 
 
 if __name__ == "__main__":
