678d3b5fde1744af37d3a175604b650d351f9263
ZXTube <ziyadedris@gmail.com>
2022-11-01 11:28:29 +0000

    Added a warning timer and 20 seconds timer always on top

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import win32gui
+import win32con
 import pygame
 import time
 import os
@@ -5,16 +7,13 @@ import os
 pygame.font.init()
 pygame.mixer.init()
 
-WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-WIDTH, HEIGHT = WIN.get_size()
-HALF_WIDTH, HALF_HEIGHT = WIDTH/2, HEIGHT/2
-
 projectFolder = os.path.dirname(__file__)
 resourcesFolder = os.path.join(projectFolder, 'Resources')
 
 FINISHED_SFX = pygame.mixer.Sound(
     os.path.join(resourcesFolder, 'Finished.wav'))
-FONT = pygame.font.SysFont('comicsans', 250)
+SMALL_FONT = pygame.font.SysFont('comicsans', 100)
+BIG_FONT = pygame.font.SysFont('comicsans', 250)
 LIGHT_GRAY = (200, 200, 200)
 DARK_GRAY = (30, 30, 30)
 
@@ -25,12 +24,10 @@ def draw(timer, timerPos):
     pygame.display.update()
 
 
-def main():
+def main(timerValue, playSoundEffect, font):
     clock = pygame.time.Clock()
-    startingTime = time.time()
 
-    timer = FONT.render('22', True, LIGHT_GRAY)
-    timerValue = 22
+    timer = font.render(str(timerValue), True, LIGHT_GRAY)
 
     halfTimerWidth, halfTimerHeight = timer.get_size()
     halfTimerHeight /= 2
@@ -49,7 +46,7 @@ def main():
                 running = False
 
         timerValue -= 1
-        timer = FONT.render(str(timerValue), True, LIGHT_GRAY)
+        timer = font.render(str(timerValue), True, LIGHT_GRAY)
         timerPos[0] = HALF_WIDTH - timer.get_width() / 2
 
         if timerValue == 0:
@@ -60,11 +57,27 @@ def main():
     pygame.display.quit()
 
     if running:
-        FINISHED_SFX.play()
+        if playSoundEffect:
+            FINISHED_SFX.play()
+        return True
+
+    return False
 
 
 if __name__ == "__main__":
     while True:
-        main()
+        WIN = pygame.display.set_mode((150, 150), pygame.NOFRAME)
+        HALF_WIDTH, HALF_HEIGHT = 75, 75
+        if not main(3, False, SMALL_FONT):
+            time.sleep(1200)
+            continue
+
+        WIN = pygame.display.set_mode((0, 0))
+        WIDTH, HEIGHT = WIN.get_size()
+        HALF_WIDTH, HALF_HEIGHT = WIDTH/2, HEIGHT/2
+
+        win32gui.SetWindowPos(pygame.display.get_wm_info()['window'], win32con.HWND_TOPMOST, 0, 0, 0, 0,
+                              win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+
+        main(20, True, BIG_FONT)
         time.sleep(1200)
-        WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
