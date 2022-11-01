0d0c819a59aeddcc532cb6bd5fabd05d61e78771
ZXTube <ziyadedris@gmail.com>
2022-11-01 20:59:34 +0000

    Added Success message in StopApp.bat
    Made setup.py only include the needed packages

---
+++ StopApp.bat
@@ -0,0 +1,5 @@
+@echo off
+
+tskill twentytwentytwenty && echo Success! Now you shouldn't get anymore 20-20-20s
+
+timeout /t 5
--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 # This script is only used to turn the main.py script into a windows executable, by running "python setup.py build"
 
 from cx_Freeze import setup, Executable
+import pkgutil
 
-files = []
+files = ["StopApp.bat", "Resources"]
+includes = ["encodings", "collections", "importlib", "pygame", "win32com", "os", "time"]
+excludes = [i.name for i in list(pkgutil.iter_modules()) if i.ispkg and i.name not in includes]
 
 target = Executable(
+    target_name="TwentyTwentyTwenty.exe",
     script="main.py",
     base="Win32GUI"
 )
 setup(
-    name="202020",
+    name="20-20-20",
     version="1.0",
     author="ZiyadCodes, https://ziyadcodes.000webhostapp.com",
     executables=[target],
-    options={'build.exe': {'include_files': files}},
-    description="202020"
+    options={"build_exe": {"include_files": files, "excludes": excludes, "optimize": 1}},  # Try optimize 2 first
+    description="20-20-20"
 )
