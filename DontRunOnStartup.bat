e3a1d25e75d230c0b98a8e3000a8c3ffa30c527c
ZXTube <ziyadedris@gmail.com>
2022-11-02 18:55:18 +0000

    Fixed spelling mistake Fixed DontRunOnStartup.bat

--- DontRunOnStartup.bat
+++ DontRunOnStartup.bat
@@ -1,5 +1,5 @@
 @echo off
 
-del "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\TwentyTwentyTwenty.exe" && echo Sucess! This app shouldn't run on startup now
+del "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\TwentyTwentyTwenty.lnk" && echo Success! This app shouldn't run on startup now
 
 timeout /t 5
